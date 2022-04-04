af9669a47dee9a29a750eea563fa676244785127
Taikhun <taiyang78190@gmail.com>
2022-04-04 12:46:21 +0200

    creation Controller with routes using attributes php8 --DONE

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,11 +3,22 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController {
     public function index() {
         $response = new Response('<h1>Hello World</h1>
         ');
         return $response;
+    }
+
+    #[
+        Route(
+            path:'/blog',
+            name: 'blog'
+        )
+    ]
+    public function blog() {
+        return new Response('Blog');
     }
 }
