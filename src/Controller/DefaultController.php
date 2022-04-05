fd96d04e4d8c1c64e3a2eb35bd1bf91a9942c8d4
Taikhun <taiyang78190@gmail.com>
2022-04-05 19:04:35 +0200

    twig chapter --DONE

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,12 +2,30 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController {
-    public function index() {
-        $response = new Response('<h1>Hello World</h1>
-        ');
-        return $response;
+class DefaultController extends AbstractController
+{   
+    #[Route('/')]
+    public function index(): Response {
+
+        $product = [
+            'name' => 'Jabra 2 evolve 65',
+            'price' => 190,
+            'lastUpdate' => strtotime('yesterday')
+        ];
+
+        return $this->render('test.html.twig', [
+            'product' => $product, 
+            'h2' => '<h2>Ceci est un test h2</h2>'
+        ]);
+    }
+
+    #[Route(path: 'about/list', name: 'about')]
+    public function aboutList()
+    {
+        return $this->render('base.html.twig');
     }
 }
